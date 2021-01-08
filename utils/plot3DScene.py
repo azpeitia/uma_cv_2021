cdba99251d0be69602d0e4bf76317365d194d5fe
Azpeitia <37340310+azpeitia@users.noreply.github.com>
2021-01-08 14:46:37 +0100

    Terminados todos los cuadernos del capitulo 8

--- utils/plot3DScene.py
+++ utils/plot3DScene.py
@@ -4,10 +4,9 @@ import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
 
 def plot3DScene(map_3d,imageRGB):
-
-    scale = 0.0002
-    map_3d = map_3d * scale
-
+    #scale = 0.0002
+    #map_3d = map_3d * scale
+    
     downsample = 8
 
     points_x = map_3d[0,:].reshape(imageRGB.shape[0],imageRGB.shape[1])
@@ -16,18 +15,18 @@ def plot3DScene(map_3d,imageRGB):
 
     rows_down = imageRGB.shape[0]/downsample
     cols_down = imageRGB.shape[1]/downsample
-    
+
     x_down = points_x[::downsample,::downsample]
     y_down = points_y[::downsample,::downsample]
     z_down = points_z[::downsample,::downsample]
-    
+
     rgb_down = imageRGB[::downsample,::downsample,:]/255
-    
+
     x_vec = x_down.reshape(int(rows_down*cols_down),1)
     y_vec = y_down.reshape(int(rows_down*cols_down),1)
     z_vec = z_down.reshape(int(rows_down*cols_down),1)
     rgb_vec = rgb_down.reshape(int(rows_down*cols_down),3)
-    
+
     # Create figure
     fig = plt.figure()
 
@@ -38,15 +37,16 @@ def plot3DScene(map_3d,imageRGB):
     ax.set_xlabel('X axis')
     ax.set_ylabel('Y axis')
     ax.set_zlabel('Z axis')
-    
+
     # Set axes limits
-    ax.set_xlim3d(-0.006,0.006)
-    ax.set_ylim3d(-0.006,0.006)
-    ax.set_zlim3d(0.006,0.016)
-    
-    ax.invert_zaxis()
-    
-    ax.view_init(elev=112, azim=-88)
+    # ax.set_xlim3d(-0.006,0.006)
+    # ax.set_ylim3d(-0.006,0.006)
+    # ax.set_zlim3d(0.006,0.016)
+
+    #ax.invert_zaxis()
+    #ax.invert_xaxis()
+
+    ax.view_init(elev=-60, azim=-90)
 
     # Plot points
     ax.scatter(x_vec,y_vec,z_vec, c=rgb_vec)
